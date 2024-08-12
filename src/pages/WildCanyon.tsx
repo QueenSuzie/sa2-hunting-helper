4142809b391ba1fe5e58a70e8f2b01d9b4b93eb8
Suzie <queen.suziex@gmail.com>
2024-08-12 00:27:59 -0400

    Refactor unnecessary function style.

--- src/pages/WildCanyon.tsx
+++ src/pages/WildCanyon.tsx
@@ -3,14 +3,14 @@ import { SetStoreFunction } from 'solid-js/store';
 import Settings from '../data/Settings';
 import PieceDB from '../components/PieceDB';
 
-const WildCanyon: Component<{search: Accessor<string>, settings: Settings, pieceDB: any, setPieceDB: SetStoreFunction<any>}> = (props) => {
-	return <PieceDB
-			stage="wild-canyon"
-			search={props.search}
-			settings={props.settings}
-			pieceDB={props.pieceDB}
-			setPieceDB={props.setPieceDB}
-		/>;
-};
+const WildCanyon: Component<{search: Accessor<string>, settings: Settings, pieceDB: any, setPieceDB: SetStoreFunction<any>}> = (props) => (
+	<PieceDB
+		stage="wild-canyon"
+		search={props.search}
+		settings={props.settings}
+		pieceDB={props.pieceDB}
+		setPieceDB={props.setPieceDB}
+	/>
+);
 
 export default WildCanyon;

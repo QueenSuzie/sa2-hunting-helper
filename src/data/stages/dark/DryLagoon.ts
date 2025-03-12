9815e4982d9d3ea4a92e6e657163b5971ff68c89
Suzie <queen.suziex@gmail.com>
2025-03-12 08:06:11 -0400

    Add first sets for DL.

--- src/data/stages/dark/DryLagoon.ts
+++ src/data/stages/dark/DryLagoon.ts
@@ -3,6 +3,14 @@ import PieceSet from "../../PieceSet";
 import PieceSets from "../../PieceSets";
 
 const sets: PieceSets = new PieceSets();
+sets.addSet("blue striped head (small oasis)", new PieceSet("behind the pillar plants", ["bulls eye!", "under turtles face"]))
+	.addPieces("behind the rubble", "under a chin (left)")
+	.addPieces("behind the water plant", "on the tilted pillar")
+	.addPieces("blue and white \"V\"", ["suns facing each other", "floating butterflies"])
+	.addPieces("blue and white striped pillar", ["big and small pillars", "up and down stairs"])
+	.addPieces("on the head (left only)", "under the turtle's face")
+	.addPieces("turtle's path (close)", "beneath the stone face apartments")
+	.addPieces("turtle's path (far)", "under a chin (left)");
 
 const codes: Code[] = [];
 for (const key of sets.keys()) {
--- src/pages/dark/DryLagoon.tsx
+++ src/pages/dark/DryLagoon.tsx
@@ -6,6 +6,7 @@ import Sets from '../../components/Sets';
 const DryLagoon: Component<{search: Accessor<string>, settings: Settings}> = (props) => {
 	return (
 		<div class="dry-lagoon-sets">
+			<div class="text-center fs-1 fw-bold">WORK IN PROGRESS</div>
 			<Sets search={props.search} settings={props.settings} sets={sets} />
 		</div>
 	);
